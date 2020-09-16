3d10d904e9a1d13b16c6e971e25186ac95736d7e
hermansteen <32453258+hermansteen@users.noreply.github.com>
2020-09-16 12:57:47 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      items:[],
+      isLoaded: false,
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/16/lat/58/data.json')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded:true,
+          items: json,
+        })
+      })
+  }
+  render() {
+    let {isLoaded, items} = this.state;
+    console.log(items.length)
+    if(!isLoaded) {
+      return <div>Loading...</div>
+    } else {
+
+      return (
+        <div className="App">
+            <ul>
+              {/* {arrayofItems.map(item => (
+                <li key={item.timeSeries.validTime}>
+                  Name: {item.timeSeries[0].parameters} | Email: {item.email}
+                </li>
+              ))} */}
+            </ul>
+        </div>
+    )
+  }
+  }
+
 }
-
 export default App;
